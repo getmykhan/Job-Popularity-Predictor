ca055abd47a678815f5ee40b4a4cddf85d85056d
getmykhan <mkhan7@stevens.edu>
2017-04-05 00:28:00 -0400

    Scrapping from careerbuilder

--- script.py
+++ script.py
@@ -1,15 +1,17 @@
-## Version 1.0
+## Version  1.0.10
 
 
 
 from bs4 import BeautifulSoup
-import re
-import requests
 import time
+import requests
+import re
+from selenium import webdriver
 
-def start(url):
-
-    pagenumber = 6
+driver = webdriver.Chrome()
+def run(url):
+    dictio=set()
+    pagenumber = 1
 
     fw = open('testdata.txt', 'w')
 
@@ -23,7 +25,7 @@ def start(url):
 
         for i in range(5): # try 5 times
             try:
-                #use the browser to access the url
+                    #use the browser to access the url
                 response=requests.get(pageLink,headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36', })
                 html=response.content # get the html
                 break # we got the file, break the loop
@@ -42,16 +44,36 @@ def start(url):
         for data in datum:
 
             test = 'NA'
-            testChunk=data.find('div',{'class': 'columns large-2 medium-3 small-12'})
+            testChunk=data.find('a',{'href': re.compile('/job/')})
             if testChunk:
                 test=testChunk.text
+                dictio.add(testChunk.get('href'))
                 print(test)
             fw.write(test+'\t')
 
             time.sleep(2)
 
     fw.close()
+    return dictio
+
+def ite(dictio):
+    url = "http://www.careerbuilder.com/jobs-data-analyst?page_number="
+    for value in dictio:
+        #print(value)
+        for i in range(5): # try 5 times
+            try:
+                #use the browser to access the url
+                response=requests.get(url + value)
+                html=response.content # get the html
+                print(html)
+                driver.get(url + value)
+                break # we got the file, break the loop
+            except Exception as e:# browser.open() threw an exception, the attempt to get the response failed
+                print ('failed attempt',i)
+                #time.sleep(2) # wait 2 secs
+
+
 
 if __name__ == "__main__":
     url = "http://www.careerbuilder.com/jobs-data-analyst?page_number="
-    start(url)
+    ite(run(url))
