acf8e9b027303f439552e9ca46ec045d43ece1ca
getmykhan <mkhan7@stevens.edu>
2017-04-04 20:48:46 -0400

    Initial Commit

--- script.py
+++ script.py
@@ -2,8 +2,42 @@
 
 
 
-
+from bs4 import BeautifulSoup
+import re
+import requests
 import time
+
+def start(url):
+
+    pagenumber = 2
+
+    fw = open('testdata.txt', 'w')
+
+    for p in range (1, pagenumber + 1):
+        print ('page',p)
+        html=None
+
+        if p==1:
+            pageLink=url # url for page 1
+        elif p == 2:
+            pageLink=url+'10' # make the page url
+
+        for i in range(5): # try 5 times
+            try:
+                #use the browser to access the url
+                response=requests.get(pageLink,headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36', })
+                html=response.content # get the html
+                break # we got the file, break the loop
+            except Exception as e:# browser.open() threw an exception, the attempt to get the response failed
+                print ('failed attempt',i)
+                time.sleep(2) # wait 2 secs
+
+        if not html:continue
+
+
+
 
 
 if __name__ == "__main__":
+    url = "https://www.indeed.com/jobs?q=business+analyst&l=New+York,+NY&start="
+    start(url)
